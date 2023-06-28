5337637d6c4ee52449bfbe3fbbfe3d2db2750143
J Duenas <134985518+JD-AI-Tech@users.noreply.github.com>
2023-06-28 00:17:18 -0400

    Update arch_assist_ui.py

--- arch_assist_ui.py
+++ arch_assist_ui.py
@@ -6,12 +6,37 @@ os.environ["OPENAI_API_KEY"] = st.secrets['apikey']
 
 aa_service = Architect_service()
 data_directory = 'data'
-# db_file_name = './db/chroma-collections.parquet'
+
 # set up needed folders
 aa_service.setup_folders()
 
 st.title("Architect AI Assistant")
-st.subheader("Artificial AI assistant for Architect")
+st.subheader("Please let me know how I can help you.")
+
+with st.sidebar:
+    st.title('About')
+    st.markdown('''
+        The goal of the Architect AI Assistant is to provide a way
+        for users to use natural language by typing their questions.
+        
+        -The Assistant will then use OpenAi's GPT-3.5 large language model to
+         interpret the questions and provide the best answer.
+        
+        -This is a Proof Of Concept (POC) and is not production ready. 
+
+     ''')
+    st.title('Technology')
+    st.markdown('''
+        Developed by Jorge Duenas using:
+        - [OpenAI GPT-3.5 API](https://openai.com/product)
+        - [Streamlit.io](https://streamlit.io/)
+        - [LangChain](https://python.langchain.com/en/latest/index.html)
+        - [Python](https://www.python.org/)
+        - [Anaconda](https://www.anaconda.com/)   
+        - [Pycharm IDE](https://www.jetbrains.com/pycharm/) 
+        - [Chroma Vector DB](https://docs.trychroma.com/)    
+
+    ''')
 
 if not does_database_exist():
     uploaded_file = st.file_uploader("Please upload file", type='pdf')
@@ -23,8 +48,8 @@ if not does_database_exist():
             aa_service.load_files_to_db()
 
 if does_database_exist():
-    user_input = st.text_area('What can I help you with?')
-    if st.button("Submit", help='Click Submit after entering your question.'):
+    user_input = st.text_area('Type in your question below and then click the Submit button.')
+    if st.button("Submit"):
         if user_input:
             with st.spinner("Please wait, looking for the best answer."):
                 returned_answer = aa_service.retrieve_data(user_input)
